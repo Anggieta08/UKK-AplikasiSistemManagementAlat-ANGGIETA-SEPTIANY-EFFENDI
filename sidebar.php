a65b1c38400a13fd4207445a3288e81075e70332
Anggieta Septiany Effendi <anggieta0809@gmail.com>
2026-04-07 18:34:23 +0700

    Add files via upload

---
+++ sidebar.php
@@ -0,0 +1,16 @@
+<div class="wrapper">
+    <div class="sidebar">
+        <div class="sidebar-brand">Dashboard Admin</div>
+        <ul>
+            <li><a href="dashboard_admin.php" class="<?php echo ($current_page == 'dashboard_admin.php') ? 'active' : ''; ?>">Dashboard</a></li>
+            <li><a href="data_alat.php" class="<?php echo ($current_page == 'data_alat.php') ? 'active' : ''; ?>">Data Alat</a></li>
+            <li><a href="data_user.php" class="<?php echo ($current_page == 'data_user.php') ? 'active' : ''; ?>">Data User</a></li>
+            <li><a href="kategori.php" class="<?php echo ($current_page == 'kategori.php') ? 'active' : ''; ?>">Kategori</a></li>
+            <li><a href="alat_masuk.php" class="<?php echo ($current_page == 'alat_masuk.php') ? 'active' : ''; ?>">Alat Masuk</a></li>
+            <li><a href="pengembalian.php" class="<?php echo ($current_page == 'pengembalian.php') ? 'active' : ''; ?>">Pengembalian</a></li>
+            <li><a href="peminjaman.php" class="<?php echo ($current_page == 'peminjaman.php') ? 'active' : ''; ?>">Peminjaman</a></li>
+            <li><a href="log_aktifitas.php" class="<?php echo ($current_page == 'log_aktifitas.php') ? 'active' : ''; ?>">Log Aktifitas</a></li>
+            <hr>
+            <li><a href="../logout.php" style="color: #d9534f;">Logout</a></li>
+        </ul>
+    </div>
